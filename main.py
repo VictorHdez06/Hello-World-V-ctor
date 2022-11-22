ca42602b36c3089edc3ea53340cd931b50be0402
VictorHdez06 <113448918+VictorHdez06@users.noreply.github.com>
2022-11-22 15:37:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#This is my first python program
+"""
+This code is written on python 3.8
+interpreted by the console
+"""
+
+print("Hello World")
+print(50)
+print(3.10)
+print(50,"letters",3.10)
+print("hello",end = ". ")
+print("world")
